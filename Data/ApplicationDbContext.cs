45be5db20669d1c159ef2a3e12608e8dbb027949
unknown <vishakvijayan2002@gmail.com>
2026-06-18 13:58:53 +0530

    setup DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions< > options) : base(options)
+        {
+            
+        }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
